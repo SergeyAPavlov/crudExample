55a8fe5377cfc79d0ed2ae1d472620a8d2c1ec06
Abax <sclavium@gmail.com>
2017-11-05 00:53:59 +0300

    create database

--- app/src/InitSite.php
+++ app/src/InitSite.php
@@ -13,20 +13,25 @@ class InitSite
 {
 
     public $sqlTables = <<<'HERE'
+-- phpMyAdmin SQL Dump
+-- version 4.7.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Ноя 05 2017 г., 00:50
+-- Версия сервера: 5.6.37
+-- Версия PHP: 7.1.7
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- База данных: `crudTest`
 --
+CREATE DATABASE IF NOT EXISTS `crudTest` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `crudTest`;
 
 -- --------------------------------------------------------
 
@@ -34,14 +39,23 @@ SET time_zone = "+00:00";
 -- Структура таблицы `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `login` varchar(16) NOT NULL,
-  `password` varchar(16) NOT NULL,
-  `fio` varchar(64) NOT NULL,
-  `email` varchar(64) NOT NULL,
+  `login` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `fio` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
   `rights` tinyint(1) NOT NULL COMMENT '0-пользователь/1-администратор'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `fio`, `email`, `rights`) VALUES
+(1, 'admin', 'password', 'Ivanov N.N', 'ivan@ivanov.ru', 1),
+(2, 'user', 'password', 'Some FIO', 'test@test.ru', 0);
 
 --
 -- Индексы сохранённых таблиц
@@ -51,16 +65,8 @@ CREATE TABLE `users` (
 -- Индексы таблицы `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE `users` ADD UNIQUE `second` (`login`);
-COMMIT;
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `second` (`login`);
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
-}
 HERE;
 }
